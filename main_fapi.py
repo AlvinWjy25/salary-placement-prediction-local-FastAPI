564e45ba1a4611397c39f8d058b3492e3d550dd0
Alvin Wijaya <Alvinwjy250@gmail.com>
2026-04-21 21:26:39 +0700

    Add files via upload

--- main_fapi.py
+++ main_fapi.py
@@ -8,8 +8,8 @@ import numpy as np
 app = FastAPI()
 
 BASE_DIR = os.getcwd()
-model_1 = joblib.load(os.path.join(BASE_DIR, 'model_classification_placement.pkl'))
-model_2 = joblib.load(os.path.join(BASE_DIR, 'model_regression_salary.pkl'))
+model_1 = joblib.load(os.path.join(BASE_DIR, 'artifacts', 'model_classification_placement.pkl'))
+model_2 = joblib.load(os.path.join(BASE_DIR, 'artifacts', 'model_regression_salary.pkl'))
 
 @app.get("/")
 def home():
